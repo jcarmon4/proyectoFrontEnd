cfdce9564737e33b8407db654e05f01e21acd808
alejocram <jcarmon4@eafit.edu.co>
2020-05-23 20:46:47 -0500

    Se modifica el endpoint del WebServices

--- measure/views.py
+++ measure/views.py
@@ -13,12 +13,12 @@ def measure(request):
         if value:
             # Crea el json para realizar la petición POST al Web Service
             args = {'type': 'ph', 'value': value}
-            response = requests.post('http://127.0.0.1:8000/measures/', args)
+            response = requests.post('http://pi1-eafit-alejocram.azurewebsites.net/measures/', args)
             # Convierte la respuesta en JSON
             measure_json = response.json()
 
     # Realiza una petición GET al Web Services
-    response = requests.get('http://127.0.0.1:8000/measures/')
+    response = requests.get('http://pi1-eafit-alejocram.azurewebsites.net/measures/')
     # Convierte la respuesta en JSON
     measures = response.json()
     # Rederiza la respuesta en el template measure
